8e27ec62679363c2ca997c8bbcd0e158c7d4e894
ameer017 <rajiabdullahi907@gmail.com>
2025-02-10 08:33:28 +0100

    using %q to add to quotations to variables

--- printingAndFormatting/main.go
+++ printingAndFormatting/main.go
@@ -1,4 +1,4 @@
-package main;
+package main
 
 import "fmt"
 
@@ -7,4 +7,20 @@ func main() {
 	fmt.Print("hello, ")
 	fmt.Print("World! \n")
 	fmt.Print("New Line")
+
+	// Println
+	fmt.Println("Hello Ninjas!")
+	fmt.Println("Goodbye Ninjas!")
+
+	// `Print` combines all text into a single line unless you include `\n` for line breaks, whereas `Println` automatically prints each statement on a new line.
+
+	age := 35
+	name := "Ninja"
+
+	fmt.Println("My name is", name, "and I am", age, "years old")
+
+	// Printf (formatted strings)
+	fmt.Printf("My name is %v and I am %v years old \n", name, age) // %v is a placeholder ==> format specifier [v === variable]
+	fmt.Printf("My name is %q and I am %q years old \n", name, age) // [q === add quotes to the variables] to be used on strings only
+	fmt.Printf("Age of type %T \n", age)
 }
