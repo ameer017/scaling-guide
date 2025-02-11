faf5ff8b7f8deeff606f6379ad7c242c37566401
ameer017 <rajiabdullahi907@gmail.com>
2025-02-11 16:03:53 +0100

    variable specifiers covered

--- printingAndFormatting/main.go
+++ printingAndFormatting/main.go
@@ -21,6 +21,12 @@ func main() {
 
 	// Printf (formatted strings)
 	fmt.Printf("My name is %v and I am %v years old \n", name, age) // %v is a placeholder ==> format specifier [v === variable]
-	fmt.Printf("My name is %q and I am %q years old \n", name, age) // [q === add quotes to the variables] to be used on strings only
-	fmt.Printf("Age of type %T \n", age)
+	fmt.Printf("My name is %q and I am %v years old \n", name, age) // [q === add quotes to the variables] to be used on strings only
+	fmt.Printf("Age is of type %T \n", age)                         // %T gets us the type of the variable
+	fmt.Printf("You scored %f poinst! \n", 225.55)                  // to return a floating value
+	fmt.Printf("You scored %0.1f poinst! \n", 225.55)               // Specifies the numbers of digits after the decimal [rounded figure!!]
+
+	// Sprintf (save formatted strings)
+	s := fmt.Sprintf("My name is %v and I am %v years old \n", name, age)
+	fmt.Println(s)
 }
