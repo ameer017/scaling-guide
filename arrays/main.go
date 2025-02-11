e81eed26c20e94f932a5de21d0f833053bb45b48
ameer017 <rajiabdullahi907@gmail.com>
2025-02-11 16:28:16 +0100

    .go slice and slice ranges

--- arrays/main.go
+++ arrays/main.go
@@ -16,6 +16,25 @@ func main() {
 	//slices [uses array under the hood]
 	var scores = []int{100, 150, 120}
 	scores[2] = 110
-	scores =append(scores, 200) // returns a new slice with the new value
+	scores = append(scores, 200) // returns a new slice with the new value
 	fmt.Println(scores)
+
+	//Slice Ranges ==> [start:end]
+	// Declare an array of fixed size 4 with string values
+	var names = [4]string{"Al Ameer", "Abdullah", "Raji", "Akintayo"}
+
+	// Create a slice from index 1 to 3 (excluding index 3)
+	rangeOne := names[1:3] // This will include "Abdullah" (index 1) and "Raji" (index 2)
+
+	// Create a slice from index 2 to the end of the array
+	rangeTwo := names[2:] // This will include "Raji" (index 2) and "Akintayo" (index 3)
+
+	// Print the slices
+	fmt.Println(rangeOne) // Output: [Abdullah Raji]
+	fmt.Println(rangeTwo) // Output: [Raji Akintayo]
+
+	rangeOne = append(rangeOne, "Alicia") // Add "Alicia" to the slice
+
+	// Print the updated slices
+	fmt.Println(rangeOne) // Output: [Abdullah Raji Alicia]
 }
