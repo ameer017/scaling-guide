18cc94ea60b5e80dea99ddc7c2bf653e69d30769
ameer017 <rajiabdullahi907@gmail.com>
2025-02-12 21:31:24 +0100

    for[while] loop

--- loops/main.go
+++ loops/main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
+	// x := 0
 
-	for x < 5 {
-		fmt.Println("Value of x is:",x)
-		x++
+	// for x < 5 {
+	// 	fmt.Println("Value of x is:",x)
+	// 	x++
+	// }
+
+	for i := 0; i < 5; i++ {
+			fmt.Println("Value of i is:",i)
+
 	}
 }
