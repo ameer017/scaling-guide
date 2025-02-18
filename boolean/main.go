eba414cedfc3d76c858149d8419188e2502fe183
ameer017 <rajiabdullahi907@gmail.com>
2025-02-18 19:27:58 +0100

    tricky conditions 👀😁

--- boolean/main.go
+++ boolean/main.go
@@ -1,8 +1,33 @@
-package main;
+package main
 
-import "fmt";
+import "fmt"
 
 func main() {
 
 	// Boolean
+	age := 45
+
+	fmt.Println(age >= 30)
+	fmt.Println(age <= 30)
+	fmt.Println(age == 45)
+	fmt.Println(age != 30)
+
+	if age < 30 {
+		fmt.Println("Age is less than 30")
+	} else if age < 40 {
+		fmt.Println("Age is less than 40")
+	} else {
+		fmt.Println("Age is greater than 40")
+	}
+
+	names := []string{"Raymond", "Reddington", "Zuma", "Elizabeth", "Tom"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+
+		fmt.Printf("The value at pos %v is %v \n", index, value)
+	}
 }
