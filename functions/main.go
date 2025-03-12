9e87238f17ea3eb9e80ab76f52079f853b95f983
ameer017 <rajiabdullahi907@gmail.com>
2025-03-12 18:00:00 +0100

    calculating the area of a circle with function dec in goLang

--- functions/main.go
+++ functions/main.go
@@ -1,8 +1,18 @@
-package main;
+package main
 
 import "fmt"
 
+func sayGreeting(n string) {
+	fmt.Printf("Good Morning %v \n", n)
+}
+
+func sayByeBye(n string) {
+	fmt.Printf("Good Bye %v \n", n)
+}
 func main() {
 
 	// Functions
+
+	sayGreeting("Raymond")
+	sayByeBye("Reddington")
 }
